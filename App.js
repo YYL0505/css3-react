baf7c6bac55d5ee2e9687854f8ff67217011b6f6
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-24 15:56:20 +0100

    YL - display: flex vs inline flex

--- App.js
+++ App.js
@@ -5,9 +5,14 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-
-            </div>
+            <nav className="main-nav">
+                <span>test</span>
+                <ul>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
         );
     }
 }
