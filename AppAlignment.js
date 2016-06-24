5424ad60a89b1afe2f0f6e70c9794c8e79a62eac
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-24 16:34:24 +0100

    YL - justify-content

---
+++ AppAlignment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AppAlignment extends React.Component {
+
+    render() {
+        return (
+            <div className="appAlignment">
+                <p className="p1 ps">this is p1</p>
+                <p className="p2 ps">this is p2</p>
+                <p className="p3 ps">this is p3</p>
+            </div>
+        );
+    }
+}
+
+export default AppAlignment
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import AppAlignment from './AppAlignment'
 
 ReactDOM.render(
-<App />,
+<AppAlignment />,
     document.getElementById('app')
 );
