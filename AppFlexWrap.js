7bb1e3dbdf1451896cf263afddcece816824207d
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-24 17:20:29 +0100

    YL - flex-wrap.

---
+++ AppFlexWrap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AppFlexWrap extends React.Component {
+
+    render() {
+        return (
+            <div className="appFlexWrap">
+                <p className="pp1 pp">this is p1</p>
+                <p className="pp2 pp">this is p2</p>
+                <p className="pp3 pp">this is p3</p>
+            </div>
+        );
+    }
+}
+
+export default AppFlexWrap
--- main.js
+++ main.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import AppAlignment from './AppAlignment'
+import AppFlexWrap from './AppFlexWrap'
 
 ReactDOM.render(
-<AppAlignment />,
+<AppFlexWrap />,
     document.getElementById('app')
 );
